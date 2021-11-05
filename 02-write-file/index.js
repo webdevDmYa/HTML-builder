77d4721dba4cc507d45c58313e913afc7cdf54c3
Dmytro Yakymenko <yakimenkodmitriy0@gmail.com>
2021-11-05 16:09:59 +0200

    fix: 02-create-file before eenter

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,33 +1,31 @@
 const fs = require('fs');
 const process = require('process');
-const readline = require('readline');
 const path = require('path');
 const stdout = process.stdout;
 const stdin = process.stdin;
-let content;
-const rl = readline.createInterface(stdin);
+let content = '';
 const stream = fs.createReadStream(__filename);
 stream.on('open', function () {
-
-	stdout.write('Welcome! Write you text:\n');
-
-	stdin.on('data', (data) => {
-		content = data.toString().trim();
-		if (content === 'exit') {
-			console.log('Good Bye!')
-			process.exit();
-		}
-		fs.open(path.dirname('text.txt'), 'a+', (err) => {
+	fs.open(path.dirname('text.txt'), 'a+', (err) => {
+		if (err) throw err;
+		fs.appendFile(path.join(__dirname, 'text.txt'), content, (err) => {
 			if (err) throw err;
+		})
+		stdout.write('Welcome! Write you text:\n');
+		stdin.on('data', (data) => {
+			content = data.toString().trim();
+			if (content === 'exit') {
+				console.log('Good Bye!')
+				process.exit();
+			}
 			fs.appendFile(path.join(__dirname, 'text.txt'), content, (err) => {
 				if (err) throw err;
 			})
+			function handle() {
+				console.log('Good Luck my friend!');
+				process.exit();
+			}
+			process.on('SIGINT', handle);
 		});
-		function handle() {
-			console.log('Good Luck my friend!');
-			process.exit();
-		}
-		process.on('SIGINT', handle);
 	});
-
 })
